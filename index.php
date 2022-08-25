2db5412a2e0b8b2b7dacd4393823cf583d2fcc98
Hailey <82413672+haileeyy@users.noreply.github.com>
2022-08-25 11:17:06 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      body{
+        background-color: black;
+        color: white;
+        -webkit-text-stroke: 3px orange;
+        font-size: 25px;
+        
+      }
+    </style>
+  </head>
+  <body>
+    whaoaa
+  </body>
+  
+</html>
